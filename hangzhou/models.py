3016d2ceccb0db90d3354aa82c3500de2214bae8
linix999 <liuqhlinix998@126.com>
2019-09-22 23:19:21 -0700

    增加字段说明注释

--- hangzhou/models.py
+++ hangzhou/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class MovieCrawlState(models.Model):
     id=models.IntegerField(primary_key=True)
     keyword = models.CharField(max_length=50)
-    status = models.IntegerField()
+    status = models.IntegerField()  #爬虫状态0：未开始，1：已开始，当爬虫开始爬时修改，不是调度了就修改
     json = models.CharField(max_length=2000, blank=True, null=True)
-    task = models.IntegerField()
-    manage = models.IntegerField()
+    task = models.IntegerField()    #任务类型 0：用户建项目，1：每天定时任务
+    manage = models.IntegerField()  #任务是否已调度，0：未调度，1：已调度
     startNum = models.IntegerField()
     finishNum = models.IntegerField()
     createtime = models.DateTimeField(db_column='createTime', blank=True, null=True)
